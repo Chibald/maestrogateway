8d34e550e712123e7775f71b6b67eaa21a988403
Anthony-55 <37302249+Anthony-55@users.noreply.github.com>
2019-10-04 21:03:23 +0200

    Ajout répertoire opt

--- install.sh
+++ install.sh
@@ -9,7 +9,11 @@ print "Installation des dépendances"
 pip install paho-mqtt
 pip install websocket-client
 print "Copie des fichiers necessaires"
-cp maestro /etc/init.d/
-chmod 0755 /etc/init.d/maestro
-systemctl daemon-reload
-update-rc.d maestro defaults
+mkdir /opt/maestro
+cp _adresses_.py /opt/maestro
+cp _data_.py /opt/maestro
+cp maestro.py /opt/maestro
+#cp maestro /etc/init.d/
+#chmod 0755 /etc/init.d/maestro
+#systemctl daemon-reload
+#update-rc.d maestro defaults
