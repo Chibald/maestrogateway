778ccb3dfb453f79f99d3658e90d4c3031346018
Anthony-55 <37302249+Anthony-55@users.noreply.github.com>
2019-10-04 20:26:06 +0200

    Create _adresses_.py

---
+++ _adresses_.py
@@ -0,0 +1,13 @@
+'''
+Fichier pour l'adressage des différents équipement
+
+'''
+
+_MCZip='192.168.120.1'
+_MCZport='81'
+_MQTT_ip='192.168.1.33'
+_MQTT_port='1883'
+_MQTT_TOPIC_SUB='jeedom2'
+_MQTT_TOPIC_PUB='jeedom'
+_VERSION='1.0'
+_AUTHOR='Anthony L.'
