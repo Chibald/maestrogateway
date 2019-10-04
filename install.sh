9f0f8c56c46b38c2229fe9f17240da766819ed80
Anthony-55 <37302249+Anthony-55@users.noreply.github.com>
2019-10-04 21:16:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,16 @@ echo "Installation des dépendances"
 pip install paho-mqtt
 pip install websocket-client
 echo "Copie des fichiers necessaires"
-mkdir /opt/maestro
+if [ ! -d "/opt/maestro" ];then
+	mkdir $DESTINATION
+	chown -R $USER $DESTINATION
+	chmod -R 0755 $DESTINATION
+fi
+
 cp _adresses_.py /opt/maestro
 cp _data_.py /opt/maestro
 cp maestro.py /opt/maestro
+
 #cp maestro /etc/init.d/
 #chmod 0755 /etc/init.d/maestro
 #systemctl daemon-reload
