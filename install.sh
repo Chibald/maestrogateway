ff3451f7af2a72b9ae61f43d742eb365e4a965a0
Anthony-55 <37302249+Anthony-55@users.noreply.github.com>
2019-10-04 19:34:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-sudo cp maestro /etc/init.d/
-sudo chmod 0755 /etc/init.d/maestro
-sudo systemctl daemon-reload
-sudo update-rc.d maestro defaults
+if [ $(whoami) != 'root' ]; then
+	echo -e "\nVous devez avoir les droits super-utilisateur pour executer $0"
+	exit 1;
+fi
+
+cp maestro /etc/init.d/
+chmod 0755 /etc/init.d/maestro
+systemctl daemon-reload
+update-rc.d maestro defaults
