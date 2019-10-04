bb9ef25fb591cf399d48f8bbfc4a8a2ca1979faf
Anthony-55 <37302249+Anthony-55@users.noreply.github.com>
2019-10-04 20:15:34 +0200

    Update install.sh
    Intsallation des dépendances nécessaires

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ if [ $(whoami) != 'root' ]; then
 	exit 1;
 fi
 
+print "Installation des dépendances"
+pip install paho-mqtt
+pip install websocket-client
+print "Copie des fichiers necessaires"
 cp maestro /etc/init.d/
 chmod 0755 /etc/init.d/maestro
 systemctl daemon-reload
