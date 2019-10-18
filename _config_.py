b615e3a0efe6e4355ac39b1f411e3cbc5f95bf34
Anthony-55 <37302249+Anthony-55@users.noreply.github.com>
2019-10-18 22:11:40 +0200

    Update _config_.py

--- _config_.py
+++ _config_.py
@@ -2,10 +2,10 @@
 Fichier pour l'adressage des différents équipement
 
 '''
-_MQTT_ip='192.168.1.33'			#Adresse IP du broker mqtt
+_MQTT_ip='127.0.0.1'			#Adresse IP du broker mqtt
 _MQTT_port=1883					#Port du broker mqtt
-_MQTT_TOPIC_SUB='jeedom2'		#Topic général de souscription
-_MQTT_TOPIC_PUB='jeedom'		#Topic général de publication
+_MQTT_TOPIC_SUB='SUBmcz'		#Topic général de souscription
+_MQTT_TOPIC_PUB='PUBmcz'		#Topic général de publication
 _MCZip='192.168.120.1'			#Adresse IP du poêle
 _MCZport='81'					#Port du serveur embarqué du poele
 _VERSION='1.0'					#Version
