d4a6515b8610645602cdb8fa2d53fe70fbba2115
Chibald <arthurchibald@gmail.com>
2019-12-19 21:01:14 +0100

    Add user and password for connectiong to broker

--- _config_.py
+++ _config_.py
@@ -6,6 +6,10 @@ Fichier pour l'adressage des différents équipement
 '''
 _MQTT_ip='127.0.0.1'				#Adresse IP du broker mqtt
 _MQTT_port=1883						#Port du broker mqtt
+_MQTT_authentication=False          # Mqtt use authentication? 
+_MQTT_user=''			            # Mqtt User name
+_MQTT_pass=''			            # Mqtt password
+_MQTT_TOPIC_PUB='PUBmcz'			#Topic général de publication
 _MQTT_TOPIC_SUB='SUBmcz'			#Topic général de souscription
 _MQTT_TOPIC_PUB='PUBmcz'			#Topic général de publication
 _MCZip='192.168.120.1'				#Adresse IP du poêle
